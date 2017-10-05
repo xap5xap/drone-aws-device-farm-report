d3da90beb765757e411431e6f55e9a8fe91d0ef0
Xavier Perez <xap5xap@users.noreply.github.com>
2017-10-05 11:25:00 -0500

    Get the run

--- plugin.go
+++ plugin.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/devicefarm"
 )
 
 // Plugin defines the Device farm plugin parameters.
@@ -18,8 +23,81 @@ type Plugin struct {
 // Exec runs the plugin
 func (p *Plugin) Exec() error {
 	fmt.Println("Begin ")
+	fmt.Println("Plugin.Key ", p.Key)
+	fmt.Println("Plugin.Secret ", p.Secret)
+	fmt.Println("Plugin.TestProject ", p.TestProject)
+	fmt.Println("Plugin.RunName ", p.RunName)
+
+	// create the configuration
+	conf := &aws.Config{
+		Region: aws.String(p.Region),
+	}
+
+	// Use key and secret if provided otherwise fall back to ec2 instance profile
+	if p.Key != "" && p.Secret != "" {
+		conf.Credentials = credentials.NewStaticCredentials(p.Key, p.Secret, "")
+	}
+	//create Device Farm service
+	svc := devicefarm.New(session.New(), conf)
+
+	//Get AWS device farm Test project
+	project, err := getTestProject(p.TestProject, svc)
+	if err != nil {
+		return err
+	}
+	//Get the run to see the status
+	run, err := getRun(p.RunName, project, svc)
+	if err != nil {
+		return err
+	}
+	fmt.Println("run", run)
 
 	fmt.Println("End")
 
 	return nil
+}
+
+func getRun(runName string, project *devicefarm.Project, svc *devicefarm.DeviceFarm) (*devicefarm.Run, error) {
+
+	var result *devicefarm.ListRunsOutput
+	var listRunInput devicefarm.ListRunsInput
+	var err error
+
+	for {
+		listRunInput.Arn = aws.String(*project.Arn)
+
+		if result != nil && result.NextToken != nil {
+			listRunInput.NextToken = aws.String(*result.NextToken)
+		}
+		result, err = svc.ListRuns(&listRunInput)
+
+		if err != nil {
+			return nil, err
+		}
+
+		for _, run := range result.Runs {
+			if *run.Name == runName {
+				return run, nil
+			}
+		}
+
+		if result.NextToken == nil {
+			return nil, fmt.Errorf("There was no Run with the name %s", runName)
+		}
+	}
+}
+
+func getTestProject(testProjectName string, svc *devicefarm.DeviceFarm) (*devicefarm.Project, error) {
+	result, err := svc.ListProjects(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, project := range result.Projects {
+		if *project.Name == testProjectName {
+			return project, nil
+		}
+	}
+
+	return nil, fmt.Errorf("There was no project with the name %s", testProjectName)
 }
